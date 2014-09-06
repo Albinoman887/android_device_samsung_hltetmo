f4b24f1b00f0525ca2d28cd79716a98eb909c05d
itsmikeramsay <ronaldramsayii@gmail.com>
2014-09-06 15:19:55 -0400

    hltetmo: Cleanup and fix up
    Change-Id: Id9f4ea494d99f12e4a5b4a0840f8e941b880177e

--- full_hltetmo.mk
+++ full_hltetmo.mk
@@ -16,7 +16,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from hlte device
-$(call inherit-product, device/samsung/hlte/device.mk)
+$(call inherit-product, device/samsung/hltetmo/device.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := full_hltetmo
